553c9cce878c040b70fa442d35ee8b081d82c476
Acec-dev <evan.chambers@utexas.edu>
2025-11-08 01:49:05 -0600

    Add files via upload

--- InventoryMenu.gd
+++ InventoryMenu.gd
@@ -4,12 +4,15 @@ extends Control
 @onready var iron_label := $Panel/VBox/IronLabel
 @onready var nickel_label := $Panel/VBox/NickelLabel
 @onready var silica_label := $Panel/VBox/SilicaLabel
+@onready var credit_label := $Panel/VBox/CreditLabel
+@onready var iron_price_label = $Panel/VBox/HBox/VBoxContainer/IronPriceLabel
+@onready var nickel_price_label = $Panel/VBox/HBox/VBoxContainer/NickelPriceLabel
+@onready var silica_price_label = $Panel/VBox/HBox/VBoxContainer/SilicaPriceLabel
 
 func _ready() -> void:
-	visible = true
+	visible = true # set to true for debug
 	GameState.inventory_changed.connect(_refresh)
 	GameState.credits_changed.connect(_refresh)
-	GameState.prices_changed.connect(_refresh)
 	sell_btn.pressed.connect(_on_sell)
 	_refresh()
 
@@ -19,13 +22,17 @@ func _input(event: InputEvent) -> void:
 		visible = not visible
 
 func _refresh(_new_credits: int = 0) -> void:
-	var iron_price = GameState.market_prices["iron"]
-	var nickel_price = GameState.market_prices["nickel"]
-	var silica_price = GameState.market_prices["silica"]
+	iron_label.text   = "Iron: %d"   % GameState.minerals["iron"]
+	nickel_label.text = "Nickel: %d" % GameState.minerals["nickel"]
+	silica_label.text = "Silica: %d" % GameState.minerals["silica"]
+	credit_label.text = "Credits: %d" % GameState.credits
+	
+	iron_price_label.text   = "Iron: $%d"   % GameState.market_prices["iron"]
+	nickel_price_label.text = "Nickel: $%d" % GameState.market_prices["nickel"]
+	silica_price_label.text = "Silica: $%d" % GameState.market_prices["silica"]
 
-	iron_label.text   = "Iron: %d (@%dc)"   % [GameState.minerals["iron"], iron_price]
-	nickel_label.text = "Nickel: %d (@%dc)" % [GameState.minerals["nickel"], nickel_price]
-	silica_label.text = "Silica: %d (@%dc)" % [GameState.minerals["silica"], silica_price]
+	
+	
 
 func _on_sell() -> void:
 	GameState.sell_all()
